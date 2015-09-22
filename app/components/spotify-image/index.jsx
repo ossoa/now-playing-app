2812c366115f39925c8c9e4b8b50b4f6331238ae
ossoa <ula.suu@gmail.com>
2015-09-22 13:17:18 +0200

    Display images for all spotify types.

--- app/components/spotify-image/index.jsx
+++ app/components/spotify-image/index.jsx
@@ -5,23 +5,30 @@ var SpotifyImage = React.createClass({
   getImageUrl: function() {
     var spotifyObj = this.props.spotify;
 
-    if (spotifyObj.type === 'track') {
-      return spotifyObj.album.images[0].url;
+    switch (spotifyObj.type) {
+      case 'track':
+        return spotifyObj.album.images[0].url;
+      case 'playlist':
+        return spotifyObj.images[0].url;
+      case 'album':
+        return spotifyObj.images[0].url;
+      case 'artist':
+        return spotifyObj.images[0].url;
+      default:
+        return false;
     }
 
-    return false;
   },
   render: function() {
     var imageUrl = this.getImageUrl();
-
+    var classString = 'spotify-image ' + this.props.spotify.type;
     var imageStyle = {};
-
     if (imageUrl) {
       imageStyle.backgroundImage ='url(' + imageUrl + ')';
     }
 
     return (
-      <div className="spotify-image" style={imageStyle}></div>
+      <div className={classString} style={imageStyle}></div>
     );
   }
 });
